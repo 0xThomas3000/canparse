18f0ed178cdb883ebb5cb5897bc0657702e49513
Thomas Ngo <thomas.ngo@ionmobility.com>
2024-05-04 16:14:31 +0700

    Get Hash list of CAN signals which correspond with CAN ID

--- src/pgn.rs
+++ src/pgn.rs
@@ -206,6 +206,21 @@ impl PgnLibrary {
             .filter_map(|pgn| pgn.1.spns.get(name))
             .next()
     }
+
+    /// Get the Hash of CAN signal by [can_id, signal_name] combination
+    pub fn hash_of_canid_signals(&self) -> HashMap<u32, Vec<&str>> {
+        let mut hash = HashMap::new();
+        for can_frame in self.pgns.iter() {
+            let canid = can_frame.1.pgn_long;
+            let mut can_signals_names = Vec::new();
+            for can_signal in can_frame.1.spns.iter() {
+                can_signals_names.push(can_signal.1.name.as_str());
+            }
+            hash.insert(canid, can_signals_names);
+        }
+
+        hash
+    }
 }
 
 impl Default for PgnLibrary {
